9489d917d55cfd5e589f113caedc01ea37129bc0
Pascal Clarkson <info@artocraft.nl>
2022-11-16 21:05:51 +0000

    feature 3

--- src/employee_listing.py
+++ src/employee_listing.py
@@ -1,14 +1,19 @@
 from src.employee_data import employee_data
+from src.employee import Employee
 
 
 class EmployeeListing:
     LISTING = employee_data
 
     def list_18_and_older(self):
-        list = [employee for employee in self.LISTING if employee["age"] >= 18]
+        list = [
+            Employee(name=employee["name"].upper(), age=employee["age"])
+            for employee in self.LISTING
+            if employee["age"] >= 18
+        ]
         list.sort(key=self.employee_name)
         return list
 
     @staticmethod
-    def employee_name(data):
-        return data["name"]
+    def employee_name(employee: Employee):
+        return employee.name
--- tests/employee_report_test.py
+++ tests/employee_report_test.py
@@ -1,13 +1,20 @@
-from expects import expect, equal
+from expects import expect, equal, match, be_above_or_equal
 from src.employee_listing import EmployeeListing
 
 
 def test_list_employees_18_and_older():
     eightteen_and_older = EmployeeListing().list_18_and_older()
-    expect(len(eightteen_and_older)).to(equal(2))
+    expect(eightteen_and_older[0].age).to(be_above_or_equal(18))
+    expect(eightteen_and_older[1].age).to(be_above_or_equal(18))
 
 
 def test_list_employees_18_and_older_sorted_by_name():
     eightteen_and_older = EmployeeListing().list_18_and_older()
-    expect(eightteen_and_older[0]["name"]).to(equal("Mike"))
-    expect(eightteen_and_older[1]["name"]).to(equal("Sepp"))
+    expect(eightteen_and_older[0].name.casefold()).to(equal("Mike".casefold()))
+    expect(eightteen_and_older[1].name.casefold()).to(equal("Sepp".casefold()))
+
+
+def test_list_employees_18_and_older_sorted_by_capitalized_name():
+    eightteen_and_older = EmployeeListing().list_18_and_older()
+    expect(eightteen_and_older[0].name).to(match(r"[A-Z]+"))
+    expect(eightteen_and_older[1].name).to(match(r"[A-Z]+"))
